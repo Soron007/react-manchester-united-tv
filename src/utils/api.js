77d4f0db9794c0478a65d6d2c6081eb7f7d8743d
Souvik Mitra <mitrasouvik123@gmail.com>
2023-06-20 09:07:01 +0530

    final prototype deploy

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key": "acc33c8cc8mshfd94f377beced11p1fd753jsnede1c3d9bd75",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
