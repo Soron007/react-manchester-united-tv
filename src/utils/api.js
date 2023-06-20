54b752895c6d956cbc9ba30628e47c1f0ea511b3
Souvik Mitra <87466601+Soron007@users.noreply.github.com>
2023-06-20 09:25:02 +0530

    Updated api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
-        "X-RapidAPI-Key": "acc33c8cc8mshfd94f377beced11p1fd753jsnede1c3d9bd75",
+        "X-RapidAPI-Key": "your_API_Key",
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
